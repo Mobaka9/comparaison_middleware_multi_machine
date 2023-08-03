5759a3366b2d3b29f4332c47c3d731ccafdc8fb5
mobaka <mohammedbakati@live.fr>
2023-08-03 16:42:49 +0200

    correction

--- main2.py
+++ main2.py
@@ -49,7 +49,7 @@ def main(nmbre_rec, protocol, message_count, port, length, flag_count, direct_ms
      #   print(f"host {hosts[i%(len(hosts))]} user: {usernames[i%(len(hosts))]}")
     for i in range(nbr_hosts):
         command = f"python3 Documents/comparaison_middleware_multi_machine/main.py --receive --protocol {protocol} --port {port} --message_count {message_count} --nbr_receivers {rec_par_hote}"
-        t=threading.Thread(target=run,args=(hosts[0],usernames[0],"bakati", [command]))
+        t=threading.Thread(target=run,args=(hosts[i],usernames[i],"", [command]))
         threads.append(t)
         threads[i].start()
     # run(hosts[0],usernames[0],"bakati",
