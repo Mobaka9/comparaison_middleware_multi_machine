403aa2b9dbc7a7c2c256596273e7a83fda9709a7
mobaka <mohammedbakati@live.fr>
2023-08-17 16:48:21 +0200

    main

--- main.py
+++ main.py
@@ -62,8 +62,8 @@ def main(nmbre_rec, protocol, message_count, port, length, flag_count, direct_ms
             
         for i in range(len(hosts)):
             rec_par_hote = runs.count(i)
-            command = f"python3 Documents/comparaison_middleware_multi_machine/main2.py --receive --protocol {protocol} --port {port} --message_count {message_count} --nbr_receivers {rec_par_hote} --ivybus_test_manager {ivybus_test_manager}"
-            t=threading.Thread(target=run,args=(hosts[i],usernames[i], "1ihm-demo", [command]))    #creation de thread pour chaque machine
+            command = f"python3 Documents/comparaison_middleware_multi_machine/main.py --receive --protocol {protocol} --port {port} --message_count {message_count} --nbr_receivers {rec_par_hote} --ivybus_test_manager {ivybus_test_manager}"
+            t=threading.Thread(target=run,args=(hosts[i],usernames[i], None, [command]))    #creation de thread pour chaque machine
             threads.append(t)
             threads[i].start()
         start_sender_and_wait(protocol, message_count, port, length, sleep, flag_count, nmbre_rec,
